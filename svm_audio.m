18344acaacc6ef1d85c2fa442c50cf5f75f38e1e
Kim Thibault <5808437+KimThibault@users.noreply.github.com>
2019-12-02 13:44:38 -0500

    Refactor variable names
    Refactor from 'dinner' and 'party' to 'genre2' and 'genre1' for future use with actual musical genres

--- svm_audio.m
+++ svm_audio.m
@@ -8,50 +8,54 @@
 
 clear ; close all; clc
 
-party_set = dlmread ('audio-features/party_audio.csv',',',1,0);
-dinner_set = dlmread ('audio-features/dinner_audio.csv',',',1,0);
+set_genre1 = dlmread ('audio-features/party_audio.csv',',',1,0);
+set_genre2 = dlmread ('audio-features/dinner_audio.csv',',',1,0);
 
-dims_party_set = size(party_set);
-n_party = dims_party_set(1);
+dims_set_genre1 = size(set_genre1);
+n_genre1 = dims_set_genre1(1);
 
-dims_dinner_set = size(dinner_set);
-n_dinner = dims_dinner_set(1);
+dims_set_genre2 = size(set_genre2);
+n_genre2 = dims_set_genre2(1);
 
-n_per_genre = min(n_party, n_dinner);
+n_per_genre = min(n_genre1, n_genre2);
 
-n_train_dinner = floor(n_per_genre * 0.8);
-n_crossval_dinner = floor(n_per_genre * 0.1);
-n_test_dinner = floor(n_per_genre * 0.1);
-n_train_party = floor(n_per_genre * 0.8);
-n_crossval_party = floor(n_per_genre * 0.1);
-n_test_party = floor(n_per_genre * 0.1);
+n_train_genre2 = floor(n_per_genre * 0.8);
+n_crossval_genre2 = floor(n_per_genre * 0.1);
+n_test_genre2 = floor(n_per_genre * 0.1);
+n_train_genre1 = floor(n_per_genre * 0.8);
+n_crossval_genre1 = floor(n_per_genre * 0.1);
+n_test_genre1 = floor(n_per_genre * 0.1);
 
-%if (n_test_dinner < 3) 
-%  'The dinner test set is too small'
-%if (n_test_party < 3) 
-%  'The party test set is too small'
+
 
 % Split sets into train, cross-validation and test data
 % Remove first row, which contains the column names
-data_train_party = party_set(2:1+n_train_party, 3:end); 
-data_test_party = party_set(n_train_party + 2 : n_train_party + n_crossval_party + n_test_party + 1, 3:end); 
+data_train_genre1 = set_genre1(2:1+n_train_genre1, 3:end); 
+data_test_genre1 = set_genre1(n_train_genre1 + 2 : n_train_genre1 + n_crossval_genre1 + n_test_genre1 + 1, 3:end); 
 
-data_train_dinner = dinner_set(1:n_train_dinner, 3:end); 
-data_test_dinner = dinner_set(n_train_dinner + n_crossval_dinner + 1 : n_train_dinner + n_crossval_dinner + n_test_dinner, 3:end); 
+data_train_genre2 = set_genre2(1:n_train_genre2, 3:end); 
+data_test_genre2 = set_genre2(n_train_genre2 + n_crossval_genre2 + 1 : n_train_genre2 + n_crossval_genre2 + n_test_genre2, 3:end); 
 
 
 %% Entrainement du modele
-n_train_per_genre = min(n_train_dinner, n_train_party);
+n_train_per_genre = min(n_train_genre2, n_train_genre1);
 % select certain features to plot 2 by 2 graph
-X = [[data_train_party(1:n_train_per_genre, 3:4)]', [data_train_dinner(1:n_train_per_genre, 3:4)]']'; 
+X = [[data_train_genre1(1:n_train_per_genre, 3:4)]', [data_train_genre2(1:n_train_per_genre, 3:4)]']'; 
+[X, mu, sigma] = featureNormalize(X);
 
 % assigne étiquette 0 à une catégorie et 1 à l'autre
 y = [zeros(1, n_train_per_genre), ones(1, n_train_per_genre)]';
 plotData(X,y);
-xlim([18 25]);
+xlim([-0.16 -0.12]);
 
 C = 10000; 
 model = svmTrain(X, y, C, @linearKernel, 1e-3, 8); 
+#w =
+#       205.1587
+#        -1.1597
+# Ca semble mauvais
+# b = 26.9 
+
 #visualizeBoundaryLinear(X, y, model); 
 
 #x1 = [1 2 1]; 
