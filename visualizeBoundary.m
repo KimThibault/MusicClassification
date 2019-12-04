4ddedc1e6f2a735ac73debb7d0ef130ef29c8e0f
Kim Thibault <5808437+KimThibault@users.noreply.github.com>
2019-12-04 15:29:36 -0500

    Refine graphical decision boundary
    Have 1000 points, not just 100, the latter is too crude

--- visualizeBoundary.m
+++ visualizeBoundary.m
@@ -5,11 +5,15 @@ function visualizeBoundary(X, y, model, varargin)
 
 % Plot the training data on top of the boundary
 plotData(X, y)
-xlim([18 25])
+xlim([-0.2 0.5]) % graphic duct tape for the Kaggle data
 
 % Make classification predictions over a grid of values
-x1plot = linspace(min(X(:,1)), max(X(:,1)), 100)';
-x2plot = linspace(min(X(:,2)), max(X(:,2)), 100)';
+#min(X(:,1))  -0.13
+#max(X(:,1))   7.4
+#min(X(:,2))  -3.6
+#max(X(:,2))  1.6
+x1plot = linspace(min(X(:,1)), max(X(:,1)), 1000)';
+x2plot = linspace(min(X(:,2)), max(X(:,2)), 1000)';
 [X1, X2] = meshgrid(x1plot, x2plot);
 vals = zeros(size(X1));
 for i = 1:size(X1, 2)
@@ -17,10 +21,17 @@ for i = 1:size(X1, 2)
    vals(:, i) = svmPredict(model, this_X);
 end
 
+#X1 varie très peu, même en considérant que les valeurs sont toutes autour de -0.13 à -0.14
+
 % Plot the SVM boundary
 hold on
-#colormap ("default");
-contour(X1, X2, vals, [0.5 0.5], 'LineColor', 'b')
+contour(X1, X2, vals, [0.5, 0.5], 'LineColor', 'b')
+%x = linspace(-10, 10, 100);
+%y = linspace(-10, 10, 100);
+%[X,Y] = meshgrid(x, y);
+%vals= X.^2 + (Y.^2);
+%contour(x,y,vals, [0.5, 0.5]);
+
 hold off;
 
 end
