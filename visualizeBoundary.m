e10d3a2767e00b3f9d6119707584ba2df6de6327
Kim Thibault <5808437+KimThibault@users.noreply.github.com>
2019-12-06 15:52:58 -0500

    WIP - studying the effect of vals
    Changed vals to be a function of X1 and X2 to understand the behaviour of contour

--- visualizeBoundary.m
+++ visualizeBoundary.m
@@ -5,15 +5,15 @@ function visualizeBoundary(X, y, model, varargin)
 
 % Plot the training data on top of the boundary
 plotData(X, y)
-xlim([-0.2 0.5]) % graphic duct tape for the Kaggle data
+xlim([-0.2 0]) % graphic duct tape for the Kaggle data
 
 % Make classification predictions over a grid of values
 #min(X(:,1))  -0.13
 #max(X(:,1))   7.4
 #min(X(:,2))  -3.6
 #max(X(:,2))  1.6
-x1plot = linspace(min(X(:,1)), max(X(:,1)), 1000)';
-x2plot = linspace(min(X(:,2)), max(X(:,2)), 1000)';
+x1plot = linspace(min(X(:,1)), max(X(:,1)), 2000)';
+x2plot = linspace(min(X(:,2)), max(X(:,2)), 2000)';
 [X1, X2] = meshgrid(x1plot, x2plot);
 vals = zeros(size(X1));
 for i = 1:size(X1, 2)
@@ -21,11 +21,11 @@ for i = 1:size(X1, 2)
    vals(:, i) = svmPredict(model, this_X);
 end
 
-#X1 varie très peu, même en considérant que les valeurs sont toutes autour de -0.13 à -0.14
 
 % Plot the SVM boundary
 hold on
-contour(X1, X2, vals, [0.5, 0.5], 'LineColor', 'b')
+vals_debug = 100*X1.^2 + (X2.^2);
+contour(X1, X2, vals_debug, [0.5, 0.5], 'LineColor', 'b')
 %x = linspace(-10, 10, 100);
 %y = linspace(-10, 10, 100);
 %[X,Y] = meshgrid(x, y);
