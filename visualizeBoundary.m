d6eae993f7fcad991c741957a7ac15288bb9cec2
Kim Thibault <5808437+KimThibault@users.noreply.github.com>
2019-12-09 17:13:34 -0500

    Removed an outlier
    Removed an outlier

--- visualizeBoundary.m
+++ visualizeBoundary.m
@@ -5,15 +5,17 @@ function visualizeBoundary(X, y, model, varargin)
 
 % Plot the training data on top of the boundary
 plotData(X, y)
-xlim([-0.2 0]) % graphic duct tape for the Kaggle data
+xlim([-0.2 0]) % graphic duct tape for the Kaggle data; TODO: generalize
 
 % Make classification predictions over a grid of values
-#min(X(:,1))  -0.13
-#max(X(:,1))   7.4
-#min(X(:,2))  -3.6
-#max(X(:,2))  1.6
-x1plot = linspace(min(X(:,1)), max(X(:,1)), 2000)';
-x2plot = linspace(min(X(:,2)), max(X(:,2)), 2000)';
+xcol1 = X(:,1);
+xcol2 = X(:,2);
+idx_to_keep = find(xcol1 < 0); # remove outlier; TODO: generalize using standard deviation
+xcol1 = xcol1(idx_to_keep);
+xcol2 = xcol2(idx_to_keep);
+
+x1plot = linspace(min(xcol1), max(xcol1), 1000)';
+x2plot = linspace(min(xcol2), max(xcol2), 1000)';
 [X1, X2] = meshgrid(x1plot, x2plot);
 vals = zeros(size(X1));
 for i = 1:size(X1, 2)
@@ -24,8 +26,11 @@ end
 
 % Plot the SVM boundary
 hold on
-vals_debug = 100*X1.^2 + (X2.^2);
-contour(X1, X2, vals_debug, [0.5, 0.5], 'LineColor', 'b')
+contour(X1, X2, vals, [0.5, 0.5], 'LineColor', 'b')
+
+#vals_debug = ((X1 - mean(X1)).^2) + ((X2 - mean(X2)).^2);
+#contour(X1, X2, vals_debug, [0:0.2:1], 'LineColor', 'b')
+
 %x = linspace(-10, 10, 100);
 %y = linspace(-10, 10, 100);
 %[X,Y] = meshgrid(x, y);
