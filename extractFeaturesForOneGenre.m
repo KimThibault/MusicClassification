1e67386883d79f318bf0a4e581d3455462aca22c
Kim Thibault <5808437+KimThibault@users.noreply.github.com>
2019-12-17 23:08:52 -0500

    Extract all 13 MFCCs
    Not just 1-4

--- extractFeaturesForOneGenre.m
+++ extractFeaturesForOneGenre.m
@@ -1,7 +1,7 @@
 function tracks_features = extractFeaturesForOneGenre(genre, n_tracks)
 
-#TODO: extend to 13 mfccs
-n_features = 4;
+#TODO: add features other than mean mfccs: std, skewness, kurtosis
+n_features = 13;
 
 tracks_features = zeros(n_tracks, n_features);
 
@@ -17,16 +17,43 @@ for i=1:n_tracks
   mfcc2 = ceps(2,:);
   mfcc3 = ceps(3,:);
   mfcc4 = ceps(4,:);
+  mfcc5 = ceps(5,:);
+  mfcc6 = ceps(6,:);
+  mfcc7 = ceps(7,:);
+  mfcc8 = ceps(8,:);
+  mfcc9 = ceps(9,:);
+  mfcc10 = ceps(10,:);
+  mfcc11 = ceps(11,:);
+  mfcc12 = ceps(12,:);
+  mfcc13 = ceps(13,:);
   
   mean_mfcc1 = mean(mfcc1);
   mean_mfcc2 = mean(mfcc2);
   mean_mfcc3 = mean(mfcc3);
   mean_mfcc4 = mean(mfcc4);
+  mean_mfcc5 = mean(mfcc5);
+  mean_mfcc6 = mean(mfcc6);
+  mean_mfcc7 = mean(mfcc7);
+  mean_mfcc8 = mean(mfcc8);
+  mean_mfcc9 = mean(mfcc9);
+  mean_mfcc10 = mean(mfcc10);
+  mean_mfcc11= mean(mfcc11);
+  mean_mfcc12 = mean(mfcc12);
+  mean_mfcc13 = mean(mfcc13);
   
   tracks_features(i,1) = mean_mfcc1;
   tracks_features(i,2) = mean_mfcc2;
   tracks_features(i,3) = mean_mfcc3;
   tracks_features(i,4) = mean_mfcc4;
+  tracks_features(i,5) = mean_mfcc5;
+  tracks_features(i,6) = mean_mfcc6;
+  tracks_features(i,7) = mean_mfcc7;
+  tracks_features(i,8) = mean_mfcc8;
+  tracks_features(i,9) = mean_mfcc9;
+  tracks_features(i,10) = mean_mfcc10;
+  tracks_features(i,11) = mean_mfcc11;
+  tracks_features(i,12) = mean_mfcc12;
+  tracks_features(i,13) = mean_mfcc13;
 end  
 
 end
